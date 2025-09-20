0e04705feee329e0b941b984ba6543c0e5a64efd
AryaWasHere <ariadewapamungkas12@gmail.com>
2025-09-20 09:24:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const NGROK_URL = 'https://508da0e3e608.ngrok-free.app'; // ganti sesuai ngrok
+const NGROK_URL = 'https://508da0e3e608.ngrok-free.app'; // ganti sesuai ngrok terbaru
 
-// Sign In
 document.getElementById('signinForm').addEventListener('submit', (e) => {
   e.preventDefault();
+
   const data = {
-    username: e.target.username.value,
-    password: e.target.password.value
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value
   };
+
   fetch(`${NGROK_URL}/signin`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   })
   .then(res => res.text())
-  .then(msg => alert(msg))
-  .catch(console.error);
+  .then(msg => {
+    alert(msg); // tes dulu, nanti bisa diarahkan ke dashboard
+  })
+  .catch(err => {
+    console.error(err);
+    alert("Terjadi error saat login!");
+  });
 });
